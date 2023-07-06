aa4ef6a8126ede175bf59e2376a8eeed097f0840
vvRams <130132301+vvRams@users.noreply.github.com>
2023-07-06 01:20:17 -0700

    Give a result for ties

--- main.js
+++ main.js
@@ -6,30 +6,35 @@ function getComputerChoice(){
 }
 function playRound(playerSelection, computerSelection){
     let result;
-    switch (computerSelection) {
-        case "rock":
-            if ((playerSelection).toLowerCase() == "paper") {
-                result = "You Win! Paper beats Rock.";
-            } else if ((playerSelection).toLowerCase() == "scissors") {
-                result = "You Lose. Rock beats Scissors.";
-            }
-            break;
-        case "paper":
-            if ((playerSelection).toLowerCase() == "scissors") {
-                result = "You Win! Scissors beats Paper.";
-            } else if ((playerSelection).toLowerCase() == "rock") {
-                result = "You Lose. Paper beats Rock.";
-            }
-            break;
-        case "scissors":
-            if ((playerSelection).toLowerCase() == "rock") {
-                result = "You Win! Rock beats Scissors.";
-            } else if ((playerSelection).toLowerCase() == "paper") {
-                result = "You Lose. Scissors beats Paper.";
-            }
-            break;
-    } 
+    if ((playerSelection).toLowerCase() == computerSelection) {
+        result = "It's a Tie.";
+    } else {
+        switch (computerSelection) {
+            case "rock":
+                if ((playerSelection).toLowerCase() == "paper") {
+                    result = "You Win! Paper beats Rock.";
+                } else if ((playerSelection).toLowerCase() == "scissors") {
+                    result = "You Lose. Rock beats Scissors.";
+                }
+                break;
+            case "paper":
+                if ((playerSelection).toLowerCase() == "scissors") {
+                    result = "You Win! Scissors beats Paper.";
+                } else if ((playerSelection).toLowerCase() == "rock") {
+                    result = "You Lose. Paper beats Rock.";
+                }
+                break;
+            case "scissors":
+                if ((playerSelection).toLowerCase() == "rock") {
+                    result = "You Win! Rock beats Scissors.";
+                } else if ((playerSelection).toLowerCase() == "paper") {
+                    result = "You Lose. Scissors beats Paper.";
+                }
+                break;
+        } 
+    }
     return result;
 }
 const computerSelection = getComputerChoice();
 const playerSelection = "paper";
+console.log(computerSelection);
