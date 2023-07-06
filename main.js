6ba3751646aa846e0501fcf3fc9a8d9575a16f80
vvRams <130132301+vvRams@users.noreply.github.com>
2023-07-06 01:02:12 -0700

    Give a result when computer has Rock

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     let randomPick = Math.floor(Math.random() * choices.length);
-    return choices[randomPick]
+    return choices[randomPick];
 }
+function playRound(playerSelection, computerSelection){
+    let result;
+    switch (computerSelection) {
+        case "rock":
+            if ((playerSelection).toLowerCase() == "paper") {
+                result = "You Win! Paper beats Rock.";
+            } else if ((playerSelection).toLowerCase() == "scissors") {
+                result = "You Lose. Rock beats Scissors."
+            }
+            break;
+    } 
+    return result
+}
+const computerSelection = getComputerChoice();
+const playerSelection = "paper";
